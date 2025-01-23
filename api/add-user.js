08756c7c25630fbe270f8de9d05419e1fbaf3bb4
RiskiSembiring <ikiputra876@gmail.com>
2025-01-23 12:47:50 +0700

    update deploy vercel

--- api/add-user.js
+++ api/add-user.js
@@ -1,19 +1,16 @@
-import express from 'express';
+// api/add-user.js
 import bcrypt from 'bcrypt';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../config/firebase-config.js';
 
-const app = express();
-app.use(express.json());
-
-// Register User
-const addUserHandler = async (req, res) => {
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
     const { username, userRole, password } = req.body;
-  
+    
     if (!username || !userRole || !password) {
       return res.status(400).json({ message: 'Semua field harus diisi!' });
     }
-  
+    
     try {
       const hashedPassword = await bcrypt.hash(password, 10);
       const docRef = await addDoc(collection(db, 'users'), { username, userRole, password: hashedPassword });
@@ -21,7 +18,7 @@ const addUserHandler = async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: 'Error saat menyimpan data', error: error.message });
     }
-};
-
-// Mengekspor handler untuk digunakan di file lain
-export { addUserHandler };
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
