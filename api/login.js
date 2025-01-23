03278707d8478cbd9a27d3467a0881131c31f4af
RiskiSembiring <ikiputra876@gmail.com>
2025-01-23 17:50:14 +0700

    update2

--- api/login.js
+++ api/login.js
@@ -6,42 +6,22 @@ import { db } from '../config/firebase-config.js';
 const loginHandler = async (req, res) => {
   const { username, password } = req.body;
 
-  // Validasi input
   if (!username || !password) {
     return res.status(400).json({ message: 'Username dan password diperlukan' });
   }
 
   try {
-    // Mencari pengguna berdasarkan username di Firestore
-    const userSnapshot = await getDocs(
-      query(collection(db, 'users'), where('username', '==', username))
-    );
-
-    // Mengambil data pengguna
+    const userSnapshot = await getDocs(query(collection(db, 'users'), where("username", "==", username)));
     const user = userSnapshot.docs.map(doc => doc.data()).find(user => user.username === username);
 
-    // Jika pengguna tidak ditemukan
-    if (!user) {
-      return res.status(404).json({ message: 'Pengguna tidak ditemukan' });
-    }
+    if (!user) return res.status(404).json({ message: 'Pengguna tidak ditemukan' });
 
-    // Verifikasi password menggunakan bcrypt
     const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Password salah' });
-    }
+    if (!passwordMatch) return res.status(401).json({ message: 'Password salah' });
 
-    // Login berhasil
-    res.status(200).json({
-      message: 'Login berhasil',
-      role: user.userRole, // Mengembalikan peran pengguna (opsional)
-    });
+    res.status(200).json({ message: 'Login berhasil', role: user.userRole });
   } catch (error) {
-    // Menangani error lainnya
-    res.status(500).json({
-      message: 'Terjadi kesalahan saat login',
-      error: error.message,
-    });
+    res.status(500).json({ message: 'Terjadi kesalahan saat login', error: error.message });
   }
 };
 
